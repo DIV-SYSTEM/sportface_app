5d1a6be58dac51bbf552296a8239d17230d629f7
Divyanshu T <tdivyanshu028@gmail.com>
2025-06-05 01:27:13 -0700

    Update user_model.dart

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,29 +1,34 @@
-// lib/models/user_model.dart
-
 class UserModel {
   final String id;
   final String name;
   final String email;
+  final String password;
+  final int? age;
+  final String? imageUrl;
 
   UserModel({
     required this.id,
     required this.name,
     required this.email,
+    required this.password,
+    this.age,
+    this.imageUrl,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      email: json['email'] as String,
-    );
-  }
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+        'password': password,
+        'age': age,
+        'image_url': imageUrl,
+      };
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-    };
-  }
+  factory UserModel.fromJson(String id, Map<String, dynamic> json) => UserModel(
+        id: id,
+        name: json['name'] as String,
+        email: json['email'] as String,
+        password: json['password'] as String,
+        age: json['age'] as int?,
+        imageUrl: json['image_url'] as String?,
+      );
 }
