a754a76883006fdb9d02ebae3c97df8031319c87
Divyanshu T <tdivyanshu028@gmail.com>
2025-06-05 04:02:52 -0700

    Create login_screen.dart

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/firebase_service.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/text_field.dart';
+import '../providers/user_provider.dart';
+import '../utils/helpers.dart';
+import 'register_screen.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() => _isLoading = true);
+      try {
+        final user = await FirebaseService().login(
+          _emailController.text,
+          _passwordController.text,
+        );
+        if (user != null) {
+          Provider.of<UserProvider>(context, listen: false).setUser(user);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Invalid email or password')),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+      setState(() => _isLoading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              CustomTextField(
+                label: 'Email',
+                controller: _emailController,
+                validator: Helpers.validateEmail,
+              ),
+              const SizedBox(height: 16),
+              CustomTextField(
+                label: 'Password',
+                controller: _passwordController,
+                obscureText: true,
+                validator: Helpers.validatePassword,
+              ),
+              const SizedBox(height: 16),
+              CustomButton(
+                text: 'Login',
+                onPressed: _login,
+                isLoading: _isLoading,
+              ),
+              TextButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const RegisterScreen()),
+                ),
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
