547cd93b3eee5bf0e60e8ccfbdbfabe4d50a926b
Divyanshu T <tdivyanshu028@gmail.com>
2025-06-11 23:52:26 -0700

    Update register_screen.dart

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
