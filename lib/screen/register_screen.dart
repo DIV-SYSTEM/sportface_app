ee9656fc0d3fc185d61debcf35c269850c61e931
Divyanshu T <tdivyanshu028@gmail.com>
2025-06-26 00:50:27 -0700

    Update register_screen.dart

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -139,11 +139,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
       );
       return;
     }
-
+ 
     setState(() => _isMatching = true);
 
     try {
-      final uri = Uri.parse('https://7466-116-73-38-158.ngrok-free.app/api/verify/');
+      final uri = Uri.parse('https://859c-116-73-38-158.ngrok-free.app/api/verify/');
       final request = http.MultipartRequest('POST', uri);
 
       request.headers['Content-Type'] = 'multipart/form-data';
